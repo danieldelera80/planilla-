f1c620d197dc268fd73edcdbbadc97be384e5325
danieldelera80 <danieldelera@gmail.com>
2026-04-01 15:45:57 -0300

    Change page title and layout in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="Sistema Centurion", layout="centered")
+st.set_page_config(page_title="Sistema Contacto", layout="centered")
 
-st.title("🏭 Control de Producción - Centurion")
+st.title("🏭 Control de Producción - Contacto")
 st.write("Seleccioná una opción para navegar:")
 
 col1, col2 = st.columns(2)
